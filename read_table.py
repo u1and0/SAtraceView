f3314d3d44210b4f8dd47b369f1a4d8f62034f32
unknown <ando@L15-505.sampanet.gr.jp>
2016-08-18 13:59:03 +0900

    Frequency column added

--- read_table.py
+++ read_table.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as pltd
 import sys
@@ -7,16 +7,25 @@ from datetime import datetime, timedelta
 import time
 
 
+# __パラメータ読み込み__________________________
+import param
+param=param.param()
+path=param['in']
+freq_start=param['freq_start']
+freq_stop=param['freq_stop']
 
-sys.path.append('../../gnuplot/SAtraceGraph/main')  #importできるディレクトリ追加
-import directoryParam as di
-path=di.in1()
 
 
 
 # __1ファイルの読み込み__________________________
 file=path+'20160101_081243.txt'
-po201601=pd.read_table(file,names=['min','ave','max'],sep='\s+',header=0,skipfooter=1,usecols=[0,1,2],engine='python')
+po201601=pd.read_table(file,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[0,1,2],engine='python')
+po201601['Frequency']=[i for i in np.linspace(freq_start,freq_stop,len(po201601))]   #frequency column added
+
+print(po201601)
+
+
+
 
 
 
@@ -24,11 +33,9 @@ po201601=pd.read_table(file,names=['min','ave','max'],sep='\s+',header=0,skipfoo
 import glob as g
 allfiles=g.glob('*.txt')
 pieces=[]
-freq_start=di.freq_start()
-freq_stop=di.freq_stop()
 for file in allfiles[:10]:
 	frame=pd.read_table(path+file,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[1,2,3],engine='python')   #1ファイルの読み込み
-	frame['Freqency']=np.arange(freq_start,freq_stop)
+	frame['Frequency']=np.arange(freq_start,freq_stop)
 	frame['Datetime']=datetime.strptime(file[:-4,'%Y%m%d_%H%M%S'])
 	pirces.append(frame)
 	data=pd.concat(pieces,ignore_index=True)
