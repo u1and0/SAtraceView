20e19d84d526310f944aaa236a24093ca9927fc3
unknown <ando@L15-505.sampanet.gr.jp>
2016-08-18 14:59:17 +0900

    compiled as function

--- read_table.py
+++ read_table.py
@@ -17,28 +17,30 @@ freq_stop=param['freq_stop']
 
 
 
-# __1ファイルの読み込み__________________________
-file='20160101_081243.txt'
-series=pd.read_table(path+file,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[1,2,3],engine='python')
-series['Frequency']=np.linspace(freq_start,freq_stop,len(series))   #frequency column added
-series['Datetime']=datetime.strptime(file[:-4],'%Y%m%d_%H%M%S')
-
-# print(series)
-
-
-
-
-
-
-# __10ファイルの読み込み__________________________
-import glob as g
-allfiles=g.glob(path+'*.txt')[:10]
-pieces=[]
-print(allfiles)
-for file in allfiles:
-	df=pd.read_table(file,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[1,2,3],engine='python')
+def onefile(fullpath):
+	'''1ファイルの読み込み'''
+	# fullpath='20160101_081243.txt'
+	df=pd.read_table(fullpath,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[1,2,3],engine='python')
 	df['Frequency']=np.linspace(freq_start,freq_stop,len(df))   #frequency column added
-	df['Datetime']=datetime.strptime(file[-19:-4],'%Y%m%d_%H%M%S')
-	pieces.append(df)
-	data=pd.concat(pieces,ignore_index=True)
-print(data)
+	df['Datetime']=datetime.strptime(fullpath[-19:-4],'%Y%m%d_%H%M%S')
+	return df
+
+
+
+
+
+
+def manyfile(start,stop):
+	'''複数ファイルの読み込み
+	'''
+	import glob as g
+	allfiles=g.glob(path+'*.txt')[start:stop]
+	pieces=[]
+	for file in allfiles:
+		pieces.append(onefile(file))
+		data=pd.concat(pieces,ignore_index=True)
+	return data
+
+
+# print(onefile(path+'20160101_081243.txt'))
+print(manyfile(None,10))
