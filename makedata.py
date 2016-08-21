b61cb8257bc15a44e714a255127b7dd31736d6cc
unknown <e01.ando60@gmail.com>
2016-08-22 00:15:57 +0900

    to_csvでファイルに吐き出し

--- makedata.py
+++ makedata.py
@@ -28,23 +28,25 @@ def gaussian(num,a=1,mu=500,sigma=50,shift=0):
 	y=a*np.exp(-(x-mu)**2/2/sigma**2)+shift
 	return y
 
-# lista=pd.Series(gaussian(1001))   #1001列のガウシアンに従ったシリーズ作る
-# listb=pd.Series(gaussian(1001,mu=400,shift=0.2))
-# listc=pd.Series(gaussian(1001,mu=800,shift=0.4))
-row=1001
-df=pd.DataFrame(list(range(row)))
-alllist=['lista' ,'listb' ,'listc']
-for listname in alllist:
-	df[listname]=pd.Series(gaussian(row,mu=row*rand(),sigma=row/10*rand(),shift=rand()))   #randomパラメータガウシアンをデータフレームに入れる
-print(df)
-df.plot(y=alllist);plt.show()
+
+def dummy_data(row):
+	df=pd.DataFrame()
+	alllist=['lista' ,'listb' ,'listc']
+	for listname in alllist:
+		df[listname]=pd.Series(gaussian(row,mu=row*rand(),sigma=row/10*rand(),shift=rand()))   #randomパラメータガウシアンをデータフレームに入れる
+	return df
+	# print(df)
+	# df.plot(y=alllist);plt.show()
 
 
 
-# start_dt=datetime(2016,2,25,0,0,23)
-# stop_dt=datetime(2016,2,25,0,55,23)
-# step_dt=timedelta(minutes=5)
-# for filename in pltd.drange(start_dt,stop_dt,step_dt):
-# 	filename='./DATA/'+pltd.num2date(filename).strftime('%Y%m%d_%H%M%S')+'.txt'
-# 	with open(filename,mode='w') as f:
-# 		f.write(randn(1000,3).tostring)
+start_dt=datetime(2016,2,25,0,0,23)
+stop_dt=datetime(2016,2,25,0,15,23)
+step_dt=timedelta(minutes=5)
+for filename in pltd.drange(start_dt,stop_dt,step_dt):
+	filename='./DATA/'+pltd.num2date(filename).strftime('%Y%m%d_%H%M%S')+'.txt'
+	df=dummy_data(1001)
+	print(df)
+	df.to_csv(path_or_buf=filename,sep=',',index_label=False,header=False)   #rowもcolumnもラベルいらない
+	# with open(filename,mode='w') as f:
+	# 	f.write(randn(1000,3).tostring)
