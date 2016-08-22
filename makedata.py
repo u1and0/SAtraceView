7c9c672620bbab0539e9dd4a2864a054e8c35ac3
unknown <ando@L15-505.sampanet.gr.jp>
2016-08-22 09:41:25 +0900

    rand()で乱数生成after all

--- makedata.py
+++ makedata.py
@@ -39,14 +39,29 @@ def dummy_data(row):
 	# df.plot(y=alllist);plt.show()
 
 
+def makefile(fullpath):
+	'''ダミーデータ書き込む'''
+	with open(fullpath,mode='w') as f:
+		c='# <This is DUMMY DATA made by %s>\n'% os.path.basename(os.getcwd())
+		for i in range(1001):
+			# for i in range(3)
+			c+=str(i).rjust(6)
+			for i in range(3):
+				c+=str(round(rand(),4)).rjust(11)
+			c+='\n'
+		c+='# <eof>\n'
+		f.write(c)
+
+
 
 start_dt=datetime(2016,2,25,0,0,23)
 stop_dt=datetime(2016,2,25,0,15,23)
 step_dt=timedelta(minutes=5)
 for filename in pltd.drange(start_dt,stop_dt,step_dt):
 	filename='./DATA/'+pltd.num2date(filename).strftime('%Y%m%d_%H%M%S')+'.txt'
-	df=dummy_data(1001)
-	print(df)
-	df.to_csv(path_or_buf=filename,sep=',',index_label=False,header=False)   #rowもcolumnもラベルいらない
+	makefile(filename)
+	# df=dummy_data(1001)
+	# print(df)
+	# df.to_csv(path_or_buf=filename,sep='\t',index_label=False,header=False)   #rowもcolumnもラベルいらない
 	# with open(filename,mode='w') as f:
 	# 	f.write(randn(1000,3).tostring)
