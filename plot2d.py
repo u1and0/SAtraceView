86b518ccca5922493b8554c5996801faadc820a3
unknown <e01.ando60@gmail.com>
2016-08-23 06:36:39 +0900

    Explanation

--- plot2d.py
+++ plot2d.py
@@ -14,9 +14,12 @@ freq_start=param['freq_start']
 freq_stop=param['freq_stop']
 num=1001
 
-## __MAIN__________________________
+## __Make DataFrame__________________________
 frequency=pd.Series(np.linspace(freq_start,freq_stop,num))   #横軸はSeriesで定義
 df=rt.glob_dataframe(rt.dataglob('20160225_12*'))
 df.index=frequency   #インデックス(横軸)を振りなおす
+
+## __PLOT1__________________________
+'''時間ごとに横軸index, 縦軸valuesでプロット'''
 df.plot()
 plt.show()   #それぞれ別のウィンドウで開く
