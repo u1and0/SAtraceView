0afe8774b9bdf3fb7f9b6adba3c48a19a71da5de
unknown <ando@L15-505.sampanet.gr.jp>
2016-08-23 13:38:35 +0900

    plot_freq_val関数化

--- plot2d.py
+++ plot2d.py
@@ -23,7 +23,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-
 ## __USER MODULES__________________________
 import read_table as rt
 import param
@@ -38,9 +37,12 @@ freq_stop=param['freq_stop']
 num=param['number_of_rows']
 outpath=param['out']
 
+## __DATA__________________________
+fullpath=None   #fullpathが空のときはdataglob()によって入力が施される
+
 ## __Make DataFrame__________________________
 frequency=pd.Series(np.linspace(freq_start,freq_stop,num))   #横軸はSeriesで定義
-df=rt.glob_dataframe(rt.dataglob())
+df=rt.glob_dataframe(rt.dataglob(fullpath))   #データフレーム
 df.index=frequency   #インデックス(横軸)を振りなおす
 
 
@@ -56,14 +58,20 @@ def plot_time_val(df):
 	trace.plot(trace[22.000])
 	plt.show()
 
+def plot_freq_val(df):
+	print('\n[グラフ化したデータ一覧]\n',df.columns)
+	##__時間ごとに横軸index, 縦軸valuesでプロット__________________________
+	df.plot(grid=True,ylim=param['ylim'],legend=False)
+	# __PLOT SETTING__________________________
+	plt.xlabel(param['xlabel'])
+	plt.ylabel(param['ylabel'])
+	if len(df.columns)<=12:   #データ12こ(1時間分)までなら凡例表示
+		plt.legend(bbox_to_anchor=(0.5, -0.25), loc='center', borderaxespad=0,fontsize='small',ncol=3)
+		plt.subplots_adjust(bottom=0.25)
+	plt.show()
 
-print('\n[グラフ化したデータ一覧]\n',df.columns)
-##__時間ごとに横軸index, 縦軸valuesでプロット__________________________
-df.plot(grid=True,ylim=param['ylim'],legend=False)
-# __PLOT SETTING__________________________
-plt.xlabel(param['xlabel'])
-plt.ylabel(param['ylabel'])
-if len(df.columns)<=12:   #データ12こ(1時間分)までなら凡例表示
-	plt.legend(bbox_to_anchor=(0.5, -0.25), loc='center', borderaxespad=0,fontsize='small',ncol=3)
-	plt.subplots_adjust(bottom=0.25)
-plt.show()
+
+'''TEST
+'''
+plot_freq_val(df)
+
