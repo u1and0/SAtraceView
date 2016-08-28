afd31549904850482a7874bc52998168e572f1ca
unknown <e01.ando60@gmail.com>
2016-08-28 14:50:35 +0900

    stats 作ってみたけどplot2d.oneplot()を直せばいいのでは

--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ from scipy import stats
 
 ## __USER MODULES__________________________
 import makedata
+
+
+
+
+df=makedata.gaussian_dataframe()
+noisef=stats.scoreatpercentile(df, 25)	#fix at 1/4median
+df['NoiseFloor']=pd.DataFrame([noisef for i in df.index])
+
+print(df)
+df.plot(ylim=(-1,1))
+plt.show()
