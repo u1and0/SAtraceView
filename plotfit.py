5491cefd44b19b5cd2a48955b193666e7d791eda
unknown <ando@L15-505.sampanet.gr.jp>
2016-08-29 10:26:34 +0900

    count_files.py:How many files in dir? plotfit.py:renamed from plot_fit.py

--- plotfit.py
+++ plotfit.py
@@ -3,17 +3,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
-import seaborn as sns
+# import seaborn as sns
 
 ## __USER MODULES__________________________
 import read_table as rt
-import param
+import makedata
 
-param=param.param()
 
-## __DATA__________________________
+# df=makedata.make_dummy_dataframe()
 df=rt.fitfile_all(param['out']+'CSV/','S????_??.csv')
 print(df)
-columns=df.columns
-prop=df[columns].count/len(df[columns[0]])
-.plot
+print('_'*20+'\n')
+
+
+print(df.count())   #全columnを集計
+print('_'*20+'\n')
+
+prop=df.count()/len(df)   #全dfに対して、いくつ値が入っているかの比率
+print(prop)
+print('_'*20+'\n')
+
+# prop.plot.bar()
+# plt.show()
+
+
+
+codf=pd.DataFrame(np.where(df,1,0),index=df.index,columns=df.columns)
+
+
+
+key=lambda x:x.date
+dfd=codf.groupby(key).sum()   #日ごとに集計
+print(dfd)
+print('_'*20+'\n')
+
+dfd.plot.bar()
+plt.show()
