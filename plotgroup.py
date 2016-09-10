b807c32fa85a291ceb788d39c323f8ea3a18ea0d
u1and0 <e01.ando60@gmail.com>
2016-09-10 14:01:02 +0900

    eachplot capsulation

--- plotgroup.py
+++ plotgroup.py
@@ -44,17 +44,23 @@ def subplot(df):
 
 
 
-def aggregate_csv(csv_fullpath,*list_of_taple):
+def aggregate_csv(csv_fullpath,list_of_tuple):
 	'''
 	dfを周波数ごとに集計
+	集計範囲はlist_of_tupleに記載
 	データフレームに取り出しやすいcsvデータから読み込む場合
+
 	引数:
-		path:Data Source
+		path: Data Source
+		list_of_tuple: 集計の日付(yyyymmddの値が二つ入ったタプル形式のリスト)
+	戻り値:
+		sub: 値は集計値(pd.DataFrame形式)
+
 	'''
 	sub=pd.DataFrame([],columns=['Temp'])
 
 	df=rt.fitfile(csv_fullpath)
-	for std,end in csvlist:
+	for std,end in list_of_tuple:
 		sub[std+'_'+end]=df.loc[std:end].max()
 
 	del sub['Temp']
@@ -63,43 +69,48 @@ def aggregate_csv(csv_fullpath,*list_of_taple):
 
 
 
+
+
+def eachplot(se, freq_list):
+	fig, ax1=plt.subplots()
+	ax1.plot(se.index, se, color='gray',linewidth=0.5)   # spectrum plot
+	for freq in freq_list:
+		se_mark= pd.Series(np.where(se.index==freq,se.ix[freq],np.nan), index= freq_index, name=param['country'][freq])   # 特定の周波数だけ値、他はNaNを返すpd.Series
+		ax1.plot(se_mark.index, se_mark, linestyle='',marker='D',markeredgewidth=1,fillstyle='none')   # 注目周波数plot as marker
+
+	plt.legend(bbox_to_anchor=(0.5, -0.25), loc='center', borderaxespad=0,fontsize='small',ncol=3)   # 別枠にラベルを書く
+	plt.subplots_adjust(bottom=0.25)
+	plt.title('SN Max in 1month %s-%s'%(column_name[:8],column_name[8:]))
+
+
 ## __MAIN__________________________
 '''
 matplotlib.pyplot.subplots()を使って、スペクトラムは黒線、注目周波数は色つきマーカーでplotする
 
-csvlist: csvから抜き出す日付(始りの日、終わりの日)yyyymmdd形式
+datelist: csvから抜き出す日付(始りの日、終わりの日)yyyymmdd形式
 freq_list: 注目周波数、param.pyに記載
 freq_index: 周波数
 '''
-csvlist=[
+datelist=[
 	('20151111','20151210'),
-	# ('20151211','20160110'),
-	# ('20160111','20160210'),
-	# ('20160211','20160310'),
-	# ('20160311','20160410'),
-	# ('20160411','20160510'),
-	# ('20160511','20160610'),
-	# ('20160611','20160710'),
-	# ('20160711','20160810'),
+	('20151211','20160110'),
+	('20160111','20160210'),
+	('20160211','20160310'),
+	('20160311','20160410'),
+	('20160411','20160510'),
+	('20160511','20160610'),
+	('20160611','20160710'),
+	('20160711','20160810'),
 	]
 
 country_keys=list(param['country'].keys())   #注目周波数
 freq_list=sorted([i for i in country_keys])   #注目周波数をタイトルに使えるようにkHz抜いた
 
-csv_fullpath=param['view_out']+'average_SN.csv'   # データソース
-df=aggregate_csv(csv_fullpath,csvlist)   #columnごとに集計を行う(max,meanなど)
+csv_fullpath=param['view_out']+'average_SN.csv'   # データソースを整理して収めたcsvファイルの場所(save_table参照)
+df=aggregate_csv(csv_fullpath,datelist)   #"csvfullpath"の中のファイルに対して、datelist(tuple of list形式)で区切って集計を行う(max,meanなど)
 
 freq_index=np.linspace(freq_start,freq_stop,freq_num)   # 周波数範囲
-title='%s_%s'%(csvlist[0][0],csvlist[0][1])
+column_name='%s_%s'%(datelist[0][0],datelist[0][1])   # dataframeのcolumn nameになる("yyyymmdd_yyyymmdd"の形のstring型)
 
-fig, ax1=plt.subplots()
-ax1.plot(df.index, df[title], color='gray',linewidth=0.5)   # spectrum plot
-for freq in freq_list:
-	df_mark= pd.Series(np.where(df[title].index==freq,df[title].ix[freq],np.nan), index= freq_index, name=param['country'][freq])   # 特定の周波数だけ値、他はNaNを返すpd.Series
-	ax1.plot(df_mark.index, df_mark, linestyle='',marker='D',markeredgewidth=1,fillstyle='none')   # 注目周波数plot as marker
-
-plt.legend(bbox_to_anchor=(0.5, -0.25), loc='center', borderaxespad=0,fontsize='small',ncol=3)   # 別枠にラベルを書く
-plt.subplots_adjust(bottom=0.25)
+eachplot(df[column_name],freq_list)
 plt.show()
-# plt.savefig(param['view_out']+'SNmax%s.png'%title,size=(5.12,2.56))
-# plt.close()
