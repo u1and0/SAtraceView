7ef92ece67ba90ce16a5330c582c3a19ae359a64
U1and0 <e01.ando60@gmail.com>
2016-09-19 15:06:23 +0900

    check pep8 at read_table.py

--- read_table.py
+++ read_table.py
@@ -1,28 +1,28 @@
-## __BUILT-IN MODULES_________________________ 
-###__SCIENCE__________________________
+# __BUILT-IN MODULES_________________________
+# __SCIENCE__________________________
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from numpy.random import *
 # import matplotlib.dates as pltd
-### __DATETIME__________________________
-from datetime import datetime, timedelta
-import time
-###__STRING__________________________
-import sys
+# __DATETIME__________________________
+# from datetime import datetime, timedelta
+# import time
+# __STRING__________________________
+# import sys
 import glob
 import codecs
 import re
-## __USER MODULES__________________________ 
+# __USER MODULES__________________________
 import param
 
-## __READ PARAMETER__________________________
-param=param.param()
+# __READ PARAMETER__________________________
+param = param.param()
 
-path=param['in']
-freq_start=param['freq_start']
-freq_stop=param['freq_stop']
-num=param['number_of_rows']
+path = param['in']
+freq_start = param['freq_start']
+freq_stop = param['freq_stop']
+num = param['number_of_rows']
 
 
 def onefile(fullpath):
@@ -31,26 +31,23 @@ def onefile(fullpath):
 	Store dataframe
 	'''
 	# fullpath='20160101_081243.txt'
-	df=pd.read_table(fullpath,names=['Min','Ave','Max'],sep='\s+',header=0,skipfooter=1,usecols=[1,2,3],engine='python')
-	df['Frequency']=np.linspace(freq_start,freq_stop,len(df))   #frequency column added
-	df['Datetime']=pd.to_datetime(fullpath[-19:-4],format='%Y%m%d_%H%M%S')
+	df = pd.read_table(fullpath, names=['Min', 'Ave', 'Max'], sep='\s+',
+	                   header=0, skipfooter=1, usecols=[1, 2, 3], engine='python')
+	df['Frequency'] = np.linspace(freq_start, freq_stop, len(df))  # frequency column added
+	df['Datetime'] = pd.to_datetime(fullpath[-19:-4], format='%Y%m%d_%H%M%S')
 	return df
 
 
-
-
-
-
-def manyfile(start,stop):
+def manyfile(start, stop):
 	'''
 	Read multiple files
 	Store dataframe
 	'''
-	allfiles=glob.glob(path+'*.txt')[start:stop]
-	pieces=[]
+	allfiles = glob.glob(path + '*.txt')[start:stop]
+	pieces = []
 	for file in allfiles:
 		pieces.append(onefile(file))
-		data=pd.concat(pieces,ignore_index=True)
+		data = pd.concat(pieces, ignore_index=True)
 	return data
 
 
@@ -58,26 +55,17 @@ def manyfile(start,stop):
 # print(manyfile(None,10))
 
 
-
-
-
-
-
-def spectrum(fullpath,columns='Ave'):
+def spectrum(fullpath, columns='Ave'):
 	'''
 	Make dataframe as ploting spectrums.
 	indexをnp.linspaceにできないかなぁ
 	'''
-	use={'Min':1,'Ave':2,'Max':3}
-	columns_name=pd.to_datetime(fullpath[-19:-4],format='%Y%m%d_%H%M%S')
-	df=pd.read_table(fullpath,names=[columns_name],sep='\s+',header=0,skipfooter=1,usecols=[use['Ave']],engine='python')
+	use = {'Min': 1, 'Ave': 2, 'Max': 3}
+	columns_name = pd.to_datetime(fullpath[-19:-4], format='%Y%m%d_%H%M%S')
+	df = pd.read_table(fullpath, names=[columns_name], sep='\s+',
+	                   header=0, skipfooter=1, usecols=[use['Ave']], engine='python')
 	# df['Frequency']=np.linspace(freq_start,freq_stop,len(df))
 	return df
-
-
-
-
-
 
 
 def read_filelist(filelist_path):
@@ -90,20 +78,16 @@ def read_filelist(filelist_path):
 	stripにより、改行、クォーテーションの削除
 	re.searchでマッチするリストの要素は内法表記で返さない
 	'''
-	code=[]
-	with codecs.open(filelist_path,'r','utf-8') as f:
-			code+=f.readlines()   #filelist.txtから1行ずつ読み込み、リストに格納
-	regex=re.compile('^#')   #regex.compileで取り除きたい要素の文字を指定
+	code = []
+	with codecs.open(filelist_path, 'r', 'utf-8') as f:
+			code += f.readlines()  # filelist.txtから1行ずつ読み込み、リストに格納
+	regex = re.compile('^#')  # regex.compileで取り除きたい要素の文字を指定
 	return [i.strip('\n\r\'\"') for i in code if not regex.search(i)]
-		   # stripにより、改行、クォーテーションの削除
-		   # re.searchでマッチするリストの要素は内法表記で返さない
+	# stripにより、改行、クォーテーションの削除
+	# re.searchでマッチするリストの要素は内法表記で返さない
 
 
-
-
-
-
-def dataglob(path,regex=False,start=0,stop=None):
+def dataglob(path, regex=False, start=0, stop=None):
 	'''
 	* 引数:
 		* regex:globするファイル名(正規表現)
@@ -136,19 +120,20 @@ ____________________________
 (例)201601??_2[13]*	<<<2016年01月のデータのうち、21時台か23時台のデータ
 ''')
 
-		print('%s内のファイルを取得します。'%path)
-		regex=input('正規表現で入力してください >> ')
+		print('%s内のファイルを取得します。' % path)
+		regex = input('正規表現で入力してください >> ')
 		if not regex:
-			return read_filelist('./filelist.txt')   #引数:読み込ませたいファイルのフルパス
+			return read_filelist('./filelist.txt')  # 引数:読み込ませたいファイルのフルパス
 		else:
-			return glob.glob(path+regex)[start:stop]
+			return glob.glob(path + regex)[start:stop]
 
-'''TEST dataglob()
+
+'''TEST dataglob
 '''
-code=[]
+code = []
 for i in dataglob(path):
-	with open(i,'r') as f:
-			code+=f.readlines()   #filelist.txtから1行ずつ読み込み、リストに格納
+	with open(i, 'r') as f:
+			code += f.readlines()  # filelist.txtから1行ずつ読み込み、リストに格納
 print(code)
 
 
@@ -168,20 +153,20 @@ def glob_dataframe(allfiles):
 	* 戻り値：
 		* df:allfilesから取得した(pandas.DataFrame形式)
 	'''
-	num=len(spectrum(allfiles[1]))   #
-	df=pd.DataFrame(list(range(num)),columns=['Temp'])   #1001要素の仮のデータフレーム作製
-	for file in allfiles:   #1ファイルを1columnとしてdfに追加
-		filebasename=file[-19:-4]
-		df[pd.to_datetime(filebasename,format='%Y%m%d_%H%M%S')]=spectrum(file)
+	num = len(spectrum(allfiles[1]))   #
+	df = pd.DataFrame(list(range(num)), columns=['Temp'])  # 1001要素の仮のデータフレーム作製
+	for file in allfiles:  # 1ファイルを1columnとしてdfに追加
+		filebasename = file[-19:-4]
+		df[pd.to_datetime(filebasename, format='%Y%m%d_%H%M%S')] = spectrum(file)
 		# df.plot(x=frequency,y=pd.Timestamp(pd.to_datetime(filebasename,format='%Y%m%d_%H%M%S')))
-	del df['Temp']   #仮で作ったデータは消す
-	print('Loading pandas DataFrame...\n\n',df)
+	del df['Temp']  # 仮で作ったデータは消す
+	print('Loading pandas DataFrame...\n\n', df)
 	print('\n\n...Loading END.\n')
 	return df
 	# plt.show()   #それぞれ別のウィンドウで開く
 
 
-def dataframe(path,regex):
+def dataframe(path, regex):
 	'''
 	glob_dataframe()からcolumn:ファイル名、index:周波数のdataframeをもらう
 	dataglob()から読み取るデータのフルパスが格納されたリストを受け取る
@@ -191,22 +176,18 @@ def dataframe(path,regex):
 	戻り値:
 		df:データフレーム(pd.DataFrame形式)
 	'''
-	frequency=pd.Series(np.linspace(freq_start,freq_stop,num))   #横軸はSeriesで定義
-	df=glob_dataframe(dataglob(path,regex))   #データフレーム;テストのときはfullpath[1], リリースのときはfullpath[0]
-	df.index=frequency   #インデックス(横軸)を振りなおす
+	frequency = pd.Series(np.linspace(freq_start, freq_stop, num))  # 横軸はSeriesで定義
+	df = glob_dataframe(dataglob(path, regex))  # データフレーム;テストのときはfullpath[1], リリースのときはfullpath[0]
+	df.index = frequency  # インデックス(横軸)を振りなおす
 	return df
-
-
-
-
 
 
 def fitfile(fullpath):
 	'''fitされた1ファイルをデータフレームとして出力'''
-	parse=lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')   #インデックスを文字列からpd.Timestamp形式に変換
-	df=pd.read_csv(fullpath,header=0,index_col='DateTime',date_parser=parse)
-			   #1行目(0行目？)をヘッダー(=columns name)とし
-			   # 'DateTime'と名前のついたcolumnをindexとする
+	parse = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')  # インデックスを文字列からpd.Timestamp形式に変換
+	df = pd.read_csv(fullpath, header=0, index_col='DateTime', date_parser=parse)
+	# 1行目(0行目？)をヘッダー(=columns name)とし
+	# 'DateTime'と名前のついたcolumnをindexとする
 	return df
 '''TEST read_fitfile()
 fullpath=param['out']+'CSV/P2015_12.csv'
@@ -216,8 +197,7 @@ print(df.index)
 '''
 
 
-
-def fitfile_all(path,regex):
+def fitfile_all(path, regex):
 	'''
 	fitされたすべてのファイル(dataglob()で取得)を行方向に追加してデータフレームを返す
 
@@ -228,12 +208,14 @@ def fitfile_all(path,regex):
 	戻り値:
 		df:path+regexで指定したすべてのファイルを行方向に連結したデータフレーム(pd.DataFrame形式)
 	'''
-	allfiles=dataglob(path,regex)
-	pieces=[]
+	allfiles = dataglob(path, regex)
+	pieces = []
 	for file in allfiles:
 		pieces.append(fitfile(file))
-		df=pd.concat(pieces)
+		df = pd.concat(pieces)
 	return df
+
+
 '''TEST fitfile_all()
 df=fitfile_all(param['out']+'CSV/','S????_??.csv')
 print(df)
