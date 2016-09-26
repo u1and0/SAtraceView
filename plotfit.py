9f3327df3913ec743b4518406d0b04af1bb38f06
u1and0 <e01.ando60@gmail.com>
2016-09-26 13:33:12 +0900

    at plotfit(), defined prop_plot, prop_plot_all, Import parameter from JSON

--- plotfit.py
+++ plotfit.py
@@ -9,10 +9,12 @@ import seaborn as sns
 
 # __USER MODULES__________________________
 import read_table as rt
-import param
-param = param.param()
+import json
+with open('parameter.json', 'r') as f:
+	param=json.load(f)
 
-df = rt.fitfile_all(param['out'] + 'CSV/', 'S????_??.csv')
+df = rt.fitfile(param['out'] + 'CSV/'+'SN20151211_20160110.csv')
+# df = rt.fitfile_all(param['out'] + 'CSV/', 'S????_??.csv')
 
 # std,end='20160111','20160210'
 # df_loc=df.loc[std:end]   #std~endまでのインデックスを選択
@@ -21,16 +23,13 @@ df = rt.fitfile_all(param['out'] + 'CSV/', 'S????_??.csv')
 def prop_plot(df_loc):
 	print('読み込んだデータフレーム')
 	print(df_loc)
-	print('_' * 20 + '\n')
 
 	print('全columnを集計')
 	print(df_loc.count())  # 全columnを集計
-	print('_' * 20 + '\n')
 
 	prop = df_loc.count() / len(df_loc)  # 全dfに対して、いくつ値が入っているかの比率
 	print('値が入っている比率')
 	print(prop)
-	print('_' * 20 + '\n')
 
 	return prop.plot.bar(title='%s-%s' % (std, end), rot=30)
 
@@ -42,15 +41,15 @@ plt.show(prop_plot(df_loc))
 
 def prop_date(df_loc):
 	df_cnt = df_loc.groupby(lambda x: x.date).count()  # 日ごとに集計
+	print('__count__')
 	print(df_cnt)
-	print('_' * 20 + '\n')
 
+	print('__sum__')
 	print(df_cnt.sum())  # 周波数ごとに合計する
-	print('_' * 20 + '\n')
 
+	print('__ratio__')
 	prop = df_cnt.sum() / len(df_loc)  # 月ごとの比率はdf_locで割り算
 	print(prop)
-	print('_' * 20 + '\n')
 	return prop
 
 
@@ -66,23 +65,42 @@ def plot_legend_setting(plot_element):
 		plt.subplots_adjust(bottom=0.25)
 
 
+def propdf(df):
+	std, end= '20151211', '20160110'
+	propdf=pd.DataFrame([])
+	propdf['%s/%s' % (std[:4], std[4:6])] = prop_date(df)
+	return propdf
+
+
+def propdf_all(df):
+	csvlist = [
+	    ('20151111', '20151210'),
+	    ('20151211', '20160110'),
+	    ('20160111', '20160210'),
+	    ('20160211', '20160310'),
+	    ('20160311', '20160410'),
+	    ('20160411', '20160510'),
+	    ('20160511', '20160610'),
+	    ('20160611', '20160710'),
+	    ('20160711', '20160810')
+	]
+	propdf = pd.DataFrame([], columns=['temp'])
+	for std, end in csvlist:
+		df_loc = df.loc[std:end]  # std~endまでのインデックスを選択
+		propdf['%s/%s' % (std[:4], std[4:6])] = prop_date(df_loc)
+	del propdf['temp']
+	return propdf
+
+
+
+
+
+
+
+
 # __MAIN__________________________
-csvlist = [
-    ('20151111', '20151210'),
-    ('20151211', '20160110'),
-    ('20160111', '20160210'),
-    ('20160211', '20160310'),
-    ('20160311', '20160410'),
-    ('20160411', '20160510'),
-    ('20160511', '20160610'),
-    ('20160611', '20160710'),
-    ('20160711', '20160810')
-]
-propdf = pd.DataFrame([], columns=['temp'])
-for std, end in csvlist:
-	df_loc = df.loc[std:end]  # std~endまでのインデックスを選択
-	propdf['%s/%s' % (std[:4], std[4:6])] = prop_date(df_loc)
-del propdf['temp']
+
+propdf=propdf(df)
 print(propdf)
 
 month_ratio = propdf.T.sort_index(axis=1)
@@ -91,4 +109,5 @@ ax = month_ratio.plot.bar(title='Monthly Reception Ratio', rot=30)
 ax.set_xlabel('Month')
 ax.set_ylabel('Ratio')
 plot_legend_setting(len(propdf.T.columns))
-plt.show(ax)
+# plt.show(ax)
+plt.savefig(param['view_out']+'allratio20151211_20160110.png')
